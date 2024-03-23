bf10aa92f08fde8107ad20f76fac180d32c48e88
firysksta <110505849+firysksta@users.noreply.github.com>
2024-03-24 00:29:38 +0500

    fix tab

--- backup.sh
+++ backup.sh
@@ -31,77 +31,77 @@ done
 
 
 function localbackup {
-if [ -n "$compression" ]
-then
-tar -czf $local/backup.tar.gz ${arrayback[*]}
-else
-for value in "${arrayback[@]}"
-do
-if [ -n "$value" ]
-then
-cp -r $value $local
-fi
-done
-fi
-echo Complete!
+  if [ -n "$compression" ]
+  then
+    tar -czf $local/backup.tar.gz ${arrayback[*]}
+  else
+    for value in "${arrayback[@]}"
+    do
+      if [ -n "$value" ]
+      then
+        cp -r $value $local
+      fi
+    done
+  fi
+  echo Complete!
 }
 
 function serverbackup {
-exec 2</dev/null
-tar -czf /home/$USER/backup.tar.gz ${arrayback[*]}
-exec 6<&2
-scp /home/$USER/backup.tar.gz "$server"
-rm -rf /home/$USER/backup.tar.gz
-echo Complete!
+  exec 2</dev/null
+  tar -czf /home/$USER/backup.tar.gz ${arrayback[*]}
+  exec 6<&2
+  scp /home/$USER/backup.tar.gz "$server"
+  rm -rf /home/$USER/backup.tar.gz
+  echo Complete!
 }
 
 function convert_to_array {
-for x in $stringi
-do
-arrayback+=($x)
-done
+  for x in $stringi
+  do
+    arrayback+=($x)
+  done
 }
 
 
 #localbackup console release
 if [ -n "$local" ]
 then
-localbackup
+  localbackup
 fi
 
 
 #serverbackup console release
 if [ -n "$server" ]
 then
-serverbackup
+  serverbackup
 fi
 
 
 #interactive mode beginning
 if [ -n "$interactive" ] 
 then
-read -p "Local backup? (yes or no): " choise
+  read -p "Local backup? (yes or no): " choise
 
-#local backup interactive
-if [ "$choise" = yes ]
-then
-read -p "Which directory to backup to? (example: /home/user): " local
-read -p "What files to backup? (Through a space): " stringi                        #Эта переменная прекрасно работает и без массива, но мне нужна была совместимость с функцией
-read -p "Place the backup in a compressed archive? (yes or no): " choisecomp       #Та же история
-convert_to_array
-if [ $choisecomp = yes ]
-then
-compression=1
-fi
-localbackup
-fi
+  #local backup interactive
+  if [ "$choise" = yes ]
+  then
+    read -p "Which directory to backup to? (example: /home/user): " local
+    read -p "What files to backup? (Through a space): " stringi                        #Эта переменная прекрасно работает и без массива, но мне нужна была совместимость с функцией
+    read -p "Place the backup in a compressed archive? (yes or no): " choisecomp       #Та же история
+    convert_to_array
+    if [ $choisecomp = yes ]
+    then
+      compression=1
+    fi
+    localbackup
+  fi
 
-#server backup interactive
-if [ "$choise" = no ]
-then
-read -p "Which server to backup to? (example user@192.168.0.1:/home/user): " server
-read -p "What files to backup? (Through a space): " stringi
-convert_to_array
-serverbackup
-fi
+  #server backup interactive
+  if [ "$choise" = no ]
+  then
+    read -p "Which server to backup to? (example user@192.168.0.1:/home/user): " server
+    read -p "What files to backup? (Through a space): " stringi
+    convert_to_array
+    serverbackup
+  fi
 fi
